f61895987d0fa8cce119dc0a23f5d2ba4d613255
amanalem <101615210+amanalem@users.noreply.github.com>
2023-03-15 23:56:06 -0400

    Time: 65 ms (54.85%), Space: 42.2 MB (69.68%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    
+    let rows = []
+    
+    rows1 = [1]
+    
+    rows2 = [1, 1]
+    
+    rows.push(rows1
+             )
+    if (numRows === 1) return rows
+    
+    rows.push(rows2)
+    
+    if (numRows === 2) return rows
+        
+    for (i = 2; i < numRows; i++) {
+            
+        let newRow = []
+        newRow.push(1)
+            
+        for (j = 1; j <= i; j++) {
+            if (j === i) {
+                newRow.push(1)
+            } else {
+                newRow.push(rows[i-1][j] + rows[i-1][j-1])
+            }
+        }
+            
+        rows.push(newRow)
+        
+    }
+    
+    return rows
+    
+    
+};
