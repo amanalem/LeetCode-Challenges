7c428bbf35870c59c36018f76072828df0c32df0
amanalem <101615210+amanalem@users.noreply.github.com>
2023-03-18 13:39:41 -0400

    Time: 69 ms (73.13%), Space: 45.4 MB (27.16%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.js
+++ 0260-single-number-iii/0260-single-number-iii.js
@@ -3,25 +3,43 @@
  * @return {number[]}
  */
 var singleNumber = function(nums) {
-    nums.sort((a,b) => a-b);
-    console.log(nums)
     
-    const res = [];
+//     ------ USING SORT METHOD ------
+//     nums.sort((a,b) => a-b);
+//     console.log(nums)
+    
+//     const res = [];
 
-    let index = 0;
+//     let index = 0;
     
-    while (index < nums.length) {
-        if (nums[index] !== nums[index + 1]) {
-            res.push(nums[index]);
-            if (res.length == 2) return res;
-            index++;
+//     while (index < nums.length) {
+//         if (nums[index] !== nums[index + 1]) {
+//             res.push(nums[index]);
+//             if (res.length == 2) return res;
+//             index++;
+//         } else {
+//             index+=2;
+//         }
+//     }
+//     ------ NOT USING SORT METHOD ------
+    const numsObj = {}
+    const res = []
+    
+    for (i = 0; i < nums.length; i++) {
+        if (numsObj[nums[i]]) {
+            numsObj[nums[i]].reps++;
         } else {
-            index+=2;
+            numsObj[nums[i]] = {value: nums[i], reps: 1};
         }
-        
-        
-        
     }
     
+    const keys = Object.keys(numsObj);
+    
+    for (i = 0; i < keys.length; i ++) {
+        if (numsObj[keys[i]].reps === 1) {
+            res.push(numsObj[keys[i]].value);
+            if (res.length === 2) return res
+        }
+    }
     
 };
