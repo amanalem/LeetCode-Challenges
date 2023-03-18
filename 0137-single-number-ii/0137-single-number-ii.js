f619ca652d5d2dd807e105ac8009ef64d3acc99b
amanalem <101615210+amanalem@users.noreply.github.com>
2023-03-18 13:30:19 -0400

    Time: 59 ms (91.96%), Space: 44.9 MB (20.29%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.js
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -3,12 +3,39 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    nums.sort((a,b) => a-b)
+//     ------ USING SORT METHOD ------
+//     nums.sort((a,b) => a-b)
     
-    for (i = 0; i < nums.length; i += 3) {
+//     for (i = 0; i < nums.length; i += 3) {
         
-        if (i === nums.length - 1) return nums[i];
-        else if (nums[i] !== nums[i+2]) return nums[i];
+//         if (i === nums.length - 1) return nums[i];
+//         else if (nums[i] !== nums[i+2]) return nums[i];
         
+//     }
+    
+    
+//     ------ NOT USING SORT METHOD -------
+    
+    let numsObj = {}
+    
+    for (i = 0; i < nums.length; i++) {
+//         if numObj num already exists add one 
+        
+        if (numsObj[nums[i]]) {
+            numsObj[nums[i]].reps++
+        }
+//         else create a property called the number and the 
+        else {
+            numsObj[nums[i]] = {value: nums[i], reps: 1}
+        }
     }
+    
+    let keys = Object.keys(numsObj)
+    
+    
+    for (i = 0; i < keys.length; i++) {
+        if (numsObj[keys[i]].reps === 1) return numsObj[keys[i]].value
+    }
+    
+
 };
