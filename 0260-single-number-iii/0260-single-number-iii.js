22b3a60cf7e30313895ce12923f3dc923135a171
amanalem <101615210+amanalem@users.noreply.github.com>
2023-03-18 13:40:16 -0400

    Time: 62 ms (88.36%), Space: 44.8 MB (43.58%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.js
+++ 0260-single-number-iii/0260-single-number-iii.js
@@ -5,41 +5,41 @@
 var singleNumber = function(nums) {
     
 //     ------ USING SORT METHOD ------
-//     nums.sort((a,b) => a-b);
-//     console.log(nums)
+    nums.sort((a,b) => a-b);
+    console.log(nums)
     
-//     const res = [];
+    const res = [];
 
-//     let index = 0;
+    let index = 0;
     
-//     while (index < nums.length) {
-//         if (nums[index] !== nums[index + 1]) {
-//             res.push(nums[index]);
-//             if (res.length == 2) return res;
-//             index++;
+    while (index < nums.length) {
+        if (nums[index] !== nums[index + 1]) {
+            res.push(nums[index]);
+            if (res.length == 2) return res;
+            index++;
+        } else {
+            index+=2;
+        }
+    }
+//     ------ NOT USING SORT METHOD ------
+//     const numsObj = {}
+//     const res = []
+    
+//     for (i = 0; i < nums.length; i++) {
+//         if (numsObj[nums[i]]) {
+//             numsObj[nums[i]].reps++;
 //         } else {
-//             index+=2;
+//             numsObj[nums[i]] = {value: nums[i], reps: 1};
 //         }
 //     }
-//     ------ NOT USING SORT METHOD ------
-    const numsObj = {}
-    const res = []
     
-    for (i = 0; i < nums.length; i++) {
-        if (numsObj[nums[i]]) {
-            numsObj[nums[i]].reps++;
-        } else {
-            numsObj[nums[i]] = {value: nums[i], reps: 1};
-        }
-    }
+//     const keys = Object.keys(numsObj);
     
-    const keys = Object.keys(numsObj);
-    
-    for (i = 0; i < keys.length; i ++) {
-        if (numsObj[keys[i]].reps === 1) {
-            res.push(numsObj[keys[i]].value);
-            if (res.length === 2) return res
-        }
-    }
+//     for (i = 0; i < keys.length; i ++) {
+//         if (numsObj[keys[i]].reps === 1) {
+//             res.push(numsObj[keys[i]].value);
+//             if (res.length === 2) return res
+//         }
+//     }
     
 };
