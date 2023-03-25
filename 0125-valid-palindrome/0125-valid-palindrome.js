12c0683dab98d4f7923a01e4388e5f1005818d68
amanalem <101615210+amanalem@users.noreply.github.com>
2023-03-25 14:14:02 -0400

    Time: 79 ms (51.54%), Space: 48.7 MB (22.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,35 +3,40 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let forward = []
+//     let forward = []
     
-    let backward = []
+//     let backward = []
     
-    let t = s.toLowerCase()
+//     let t = s.toLowerCase()
     
-    for (let i = 0; i < s.length; i ++){
-        if (t.charCodeAt(i) < 123 && t.charCodeAt(i) > 96) {
-            forward.push(t[i])
-            backward.splice(0, 0, t[i])
-        } else if (t.charCodeAt(i) < 58 && t.charCodeAt(i) > 47) {
-            forward.push(t[i])
-            backward.splice(0, 0, t[i])
-        }
-    }
+//     for (let i = 0; i < s.length; i ++){
+//         if (t.charCodeAt(i) < 123 && t.charCodeAt(i) > 96) {
+//             forward.push(t[i])
+//             backward.splice(0, 0, t[i])
+//         } else if (t.charCodeAt(i) < 58 && t.charCodeAt(i) > 47) {
+//             forward.push(t[i])
+//             backward.splice(0, 0, t[i])
+//         }
+//     }
 
-    forward = forward.join('')
-    backward = backward.join('')
+//     let f = forward.join('')
+//     let b = backward.join('')
     
   
     
-    if (forward === backward) {
-        return true
-    } else {
-        return false
+//     if (f === b) {
+//         return true
+//     } else {
+//         return false
+//     }
+    
+    let str = s.match(/[A-Za-z0-9]/g)
+    if (str === null) return true
+    str = str.join('').toLowerCase()
+    
+    for (i = 0; i < str.length/2; i++) {
+        if (str.charAt(i) !== str.charAt(str.length - i - 1)) return false
     }
     
-    // let str = s.match(/[A-Za-z0-9]/g)
-    // if (str === null) return true
-    // str = str.join('')
-    
+    return true
 };
