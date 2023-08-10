91e6d164dcd6afb52ce14202ffcb75547a35a3c2
amanalem <101615210+amanalem@users.noreply.github.com>
2023-08-10 11:31:37 -0400

    Time: 205 ms (8.07%), Space: 42.1 MB (83.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,26 +4,26 @@
  * @return {number[]}
  */
 
-// const twoSum = (nums, target) => {
-//     for (i = 0; i < nums.length; i++) {
-//         let indexComp = i
-//         for (j = 0; j < nums.length; j++) {
-//             if (j != indexComp) {
-//                 if (nums[j] + nums[indexComp] == target) {
-//                     return [indexComp, j]
-//                 }
-//             }
-//         }
-//     }
-// };
-
 const twoSum = (nums, target) => {
     for (i = 0; i < nums.length; i++) {
-        let num = target - nums[i]
-        for (j = 0; j < nums.length; j++){
-           if (j!==i) {
-               if (nums[j] == num) return [i, j]
-           }           
+        let indexComp = i
+        for (j = 0; j < nums.length; j++) {
+            if (j != indexComp) {
+                if (nums[j] + nums[indexComp] == target) {
+                    return [indexComp, j]
+                }
+            }
         }
     }
 };
+
+// const twoSum = (nums, target) => {
+//     for (i = 0; i < nums.length; i++) {
+//         let num = target - nums[i]
+//         for (j = 0; j < nums.length; j++){
+//            if (j!==i) {
+//                if (nums[j] == num) return [i, j]
+//            }           
+//         }
+//     }
+// };
