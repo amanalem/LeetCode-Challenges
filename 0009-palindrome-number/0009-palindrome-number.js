5357b6adec65e40a797b524cf9cf304f024c091e
amanalem <101615210+amanalem@users.noreply.github.com>
2023-08-11 14:04:03 -0400

    Time: 142 ms (88.55%), Space: 51 MB (64.65%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,18 +2,30 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    let y = x.toString()
-    let z = ''
+// var isPalindrome = function(x) {
+//     let y = x.toString()
+//     let z = ''
     
-    for (i = y.length - 1; i > -1; i-=1) {
-        z = z + y[i];
-    }
+//     for (i = y.length - 1; i > -1; i-=1) {
+//         z = z + y[i];
+//     }
     
-    if (y === z) {
-        return true
-    } else {
-        return false
-    }
+//     if (y === z) {
+//         return true
+//     } else {
+//         return false
+//     }
     
+// };
+
+
+let isPalindrome = (x) => {
+    let arr = x.toString().split("")
+    if (arr[0] !== "-") {
+        for (i = 0; i <= (arr.length-1) / 2; i++) {
+            if (arr[i] !== arr[arr.length-1-i]) return false
+        }
+    } else return false
+    
+    return true
 };
